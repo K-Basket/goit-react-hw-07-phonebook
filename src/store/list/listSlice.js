31d12c00a5008199d592b3e5fba33c44e19dc1eb
K-Basket <koshykkv@gmail.com>
2023-06-18 15:27:26 +0300

    add method with extraReducers

--- src/store/list/listSlice.js
+++ src/store/list/listSlice.js
@@ -1,19 +1,24 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { getContacts } from 'api/contactsApi';
 // import { allSelector } from './selectorsList';
 
-// ---temp: getProductsThunk
-export const getContactsThunk = () => {
-  return async dispatch => {
-    dispatch(listSlice.actions.fetching());
-    try {
-      const data = await getContacts(); // Запрос на backend
-      dispatch(listSlice.actions.fetchSuccess(data));
-    } catch (error) {
-      dispatch(listSlice.actions.fetchError(error));
-    }
-  };
-};
+// Option 1
+// export const getContactsThunk = () => {
+//   return async dispatch => {
+//     dispatch(listSlice.actions.fetching());
+//     try {
+//       const data = await getContacts(); // Запрос на backend
+//       dispatch(listSlice.actions.fetchSuccess(data));
+//     } catch (error) {
+//       dispatch(listSlice.actions.fetchError(error));
+//     }
+//   };
+// };
+
+// Option 2
+export const getContactsThunk = createAsyncThunk('list/getAllContacts', () => {
+  return getContacts(); // Запрос на backend
+});
 
 const initialState = {
   items: [],
@@ -22,59 +27,112 @@ const initialState = {
   filter: '',
 };
 
-// const initialState = {
-//   contacts: {
-//     items: [],
-//     isLoading: false,
-//     error: null,
-//   },
-//   filter: '',
-// };
-
+// Option 2
 export const listSlice = createSlice({
   name: 'list',
   initialState: initialState,
-  reducers: {
-    fetching: state => {
+  extraReducers: {
+    [getContactsThunk.pending]: state => {
       state.isLoading = true;
     },
-    fetchSuccess: (state, { payload }) => {
+    [getContactsThunk.fulfilled]: (state, { payload }) => {
       state.isLoading = false;
       state.items = payload;
       state.error = '';
     },
-    fetchError: (state, { payload }) => {
+    [getContactsThunk.rejected]: (state, { payload }) => {
       state.isLoading = false;
       state.error = payload;
     },
-
-    fetchContacts: {},
-    addContact: {},
-    // deleteContact: {},
-
-    setContacts: {
-      reducer(state, action) {
-        state.contacts.push(action.payload);
-      },
-      prepare(name, number) {
-        return {
-          payload: {
-            id: nanoid(),
-            name: name,
-            number: number,
-          },
-        };
-      },
-    },
-    deleteContact: (state, action) => {
-      state.contacts = state.contacts.filter(el => el.id !== action.payload);
-    },
-
-    setFilter: (state, action) => {
-      state.filter = action.payload;
-    },
   },
+
+  // reducers: {
+  //   fetching: state => {
+  //     state.isLoading = true;
+  //   },
+  //   fetchSuccess: (state, { payload }) => {
+  //     state.isLoading = false;
+  //     state.items = payload;
+  //     state.error = '';
+  //   },
+  //   fetchError: (state, { payload }) => {
+  //     state.isLoading = false;
+  //     state.error = payload;
+  //   },
+
+  //   fetchContacts: {},
+  //   addContact: {},
+  //   deleteContact: {},
+
+  //   setContacts: {
+  //     reducer(state, action) {
+  //       state.contacts.push(action.payload);
+  //     },
+  //     prepare(name, number) {
+  //       return {
+  //         payload: {
+  //           id: nanoid(),
+  //           name: name,
+  //           number: number,
+  //         },
+  //       };
+  //     },
+  //   },
+  //   deleteContact: (state, action) => {
+  //     state.contacts = state.contacts.filter(el => el.id !== action.payload);
+  //   },
+
+  //   setFilter: (state, action) => {
+  //     state.filter = action.payload;
+  //   },
+  // },
 });
+
+// Option 1
+// export const listSlice = createSlice({
+//   name: 'list',
+//   initialState: initialState,
+//   reducers: {
+//     fetching: state => {
+//       state.isLoading = true;
+//     },
+//     fetchSuccess: (state, { payload }) => {
+//       state.isLoading = false;
+//       state.items = payload;
+//       state.error = '';
+//     },
+//     fetchError: (state, { payload }) => {
+//       state.isLoading = false;
+//       state.error = payload;
+//     },
+
+//     fetchContacts: {},
+//     addContact: {},
+//     deleteContact: {},
+
+//     setContacts: {
+//       reducer(state, action) {
+//         state.contacts.push(action.payload);
+//       },
+//       prepare(name, number) {
+//         return {
+//           payload: {
+//             id: nanoid(),
+//             name: name,
+//             number: number,
+//           },
+//         };
+//       },
+//     },
+//     deleteContact: (state, action) => {
+//       state.contacts = state.contacts.filter(el => el.id !== action.payload);
+//     },
+
+//     setFilter: (state, action) => {
+//       state.filter = action.payload;
+//     },
+//   },
+// });
 
 export const listReducer = listSlice.reducer;
 export const { setContacts, setFilter, deleteContact } = listSlice.actions;
